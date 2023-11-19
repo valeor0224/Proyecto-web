2732bb28a08ac7c1c0c861aaf10663efeef787d5
valeor0224 <00206022@uca.edu.sv>
2023-11-18 21:32:29 -0600

    update in BowserRouter import

--- proyecto-equipo-13-sec-01-powerpuffs-client/src/App.jsx
+++ proyecto-equipo-13-sec-01-powerpuffs-client/src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
-    </Router>
+    </BrowserRouter>
     
   );
 }
