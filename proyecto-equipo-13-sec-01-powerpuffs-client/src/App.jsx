1aebb95eb1a5779929a3ecc24faff3c8dd8a9f97
valeor0224 <00206022@uca.edu.sv>
2023-11-18 22:23:18 -0600

    Adopta folder and route

--- proyecto-equipo-13-sec-01-powerpuffs-client/src/App.jsx
+++ proyecto-equipo-13-sec-01-powerpuffs-client/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Adopta from './pages/Adopta/Adopta';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="Adopta" element={<Adopta />} />
       </Routes>
       <Footer />
     </BrowserRouter>
