40cfa06c0ec1a9d98ed0892aafdccf01cb658372
valeor0224 <00206022@uca.edu.sv>
2023-11-29 22:07:39 -0600

    Agregar info de display

--- proyecto-equipo-13-sec-01-powerpuffs-client/src/components/DisplayAdopAdmin/DisplayAdopAdmin.jsx
+++ proyecto-equipo-13-sec-01-powerpuffs-client/src/components/DisplayAdopAdmin/DisplayAdopAdmin.jsx
@@ -14,6 +14,33 @@ const DisplayAdopAdmin = ({ data, onClose }) => {
       <p>Name: {data.nombre}</p>
       <p>Submission date: {data.submissionDate}</p>
       {/* Add more details based on your data structure */}
+
+      <button className='view-formulario'>
+        Ver formulario de adopcion
+      </button>
+
+      <div className='cita-visita'>
+        <label htmlFor="fecha-cita">Fecha de cita:</label>
+        <input type="date" id="fecha-cita" name="fechaCita" />
+
+        <label for="meeting-time">Hora cita:</label>
+
+        <input type="time" id="meeting-time" name="meeting-time" min="09:00" max="18:00" required />
+
+      </div>
+
+      <div className='resolucion-adopcion'>
+        <p>Resolución</p>
+        <select id="Educacion" name="educacion" >
+          <option value="pendiente">Pendiente</option>
+          <option value="aprobado">Aprobado</option>
+          <option value="denegado">Denegado</option>
+        </select>
+        <input type="text" id="resolucion-input" name="resolucion-input" placeholder="Motivo de la resolucion" />
+       
+      </div>
+
+
       <button onClick={onClose}>Close</button>
     </div>
   );
