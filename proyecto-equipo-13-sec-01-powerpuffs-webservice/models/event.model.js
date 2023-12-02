f3c106a3ca5e4b79e1a738c78945f34950d4cac5
alexandrappe <00117222@uca.edu.sv>
2023-12-01 20:12:55 -0600

    Add event model :sparkles:

---
+++ proyecto-equipo-13-sec-01-powerpuffs-webservice/models/event.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; 
+
+const PetSchema = new Schema({
+title:{
+    type:String,
+    trim:true,
+    required:true
+},
+description:{
+    type:String,
+    trim:true,
+    required:true
+},
+publication_date:{
+    type:Date,
+   // default:Date.now
+},
+image:{
+    //URL de la imagen
+    type:String,
+    trim:true
+},
+label:{
+    type:String,
+    trim:true,
+    required:true
+},
+
+publication_zone:{
+    type:String,
+    trim:true,
+},
+
+author_name:{
+    type:String,
+    trim:true,
+},
+
+hidden:{
+type:Boolean,
+default: false
+},
+},{ timestamps:true});
+
+module.exports = mongoose.model("event", PetSchema);
