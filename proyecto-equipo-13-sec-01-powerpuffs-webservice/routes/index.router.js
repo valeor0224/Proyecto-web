83676dcfceb6395f6f1c030a6be2544c4fa47ef8
alexandrappe <00117222@uca.edu.sv>
2023-12-02 00:01:47 -0600

    Update index router :construction:

--- proyecto-equipo-13-sec-01-powerpuffs-webservice/routes/index.router.js
+++ proyecto-equipo-13-sec-01-powerpuffs-webservice/routes/index.router.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const articleRouter = require("./article.router");
 const authRouter = require ("./auth.router");
+const eventRouther = require("./event.router");
 
 router.use("/auth", authRouter);
 router.use("/article", articleRouter);
-
+router.use("/event", eventRouther);
 
 module.exports = router;
