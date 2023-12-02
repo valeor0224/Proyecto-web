69c855799958bb0baa3d8a7d876d8206c498adbb
alexandrappe <00117222@uca.edu.sv>
2023-12-01 23:59:46 -0600

    Add event router :sparkles:

---
+++ proyecto-equipo-13-sec-01-powerpuffs-webservice/routes/event.router.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+const { createEventValidator, labelInParamsValidator, idInParamsValidator } = require("../validators/event.validators");
+const validateFields = require("../validators/index.middleware");
+
+const { authentication } = require("../middlewares/auth.middlewares");
+
+const eventController = require("../controllers/event.controller");
+
+
+//llegan desde /event/....
+router.get("/event", eventController.findAll);
+
+router.get("/looking/event/:identifier",
+    labelInParamsValidator,
+    validateFields,
+    eventController.findOneByLabel);
+
+router.get("/find/event/:id",
+    idInParamsValidator,
+    validateFields,
+    eventController.findOneById);
+
+
+router.post(["/post/", "/event/:id"],
+    authentication,
+    createEventValidator,
+    validateFields,
+    eventController.save);
+
+router.delete("/delete/:id",
+    idInParamsValidator,
+    validateFields,
+    eventController.deleteById);
+
+module.exports = router;
+
